e12e221c1d72d748fad80976b25c1879899b5371
Seongjun2 <tjdwns4302@naver.com>
2016-01-24 15:36:28 +0900

    걍

--- app/src/main/java/com/example/takoyaki/newshyboys/MainActivity.java
+++ app/src/main/java/com/example/takoyaki/newshyboys/MainActivity.java
@@ -74,8 +74,7 @@ public class MainActivity extends AppCompatActivity
 
         setTitle("");
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
--- app/src/main/java/com/example/takoyaki/newshyboys/ProfessorActivity.java
+++ app/src/main/java/com/example/takoyaki/newshyboys/ProfessorActivity.java
@@ -80,7 +80,6 @@ public class ProfessorActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         setTitle("");
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
